b7618b964ac2a811428f6e5368564de20fad7aa9
melatigoldgallery <melatigoldshopid@gmail.com>
2025-01-26 11:55:12 +0800

    melakukan perubahan pada audioHandler dan audioConfig

--- js/audioConfig.js
+++ js/audioConfig.js
@@ -1,5 +1,6 @@
 export const AUDIO_PATHS = {
     informasi: "audio/informasi.mp3",
-    antrian: "audio/antrian.mp3"
+    antrian: "audio/antrian.mp3",
+    informasiEnd : "audio/informasiEnd.mp3"
 };
 
