de4b73123aca8ca28b2a170a26b5d5f935bb5e98
melatigoldgallery <melatigoldshopid@gmail.com>
2025-02-20 13:38:30 +0800

    perbaikan deploy github

--- js/auth/authCheck.js
+++ js/auth/authCheck.js
@@ -2,7 +2,7 @@ import { authService } from './../configFirebase.js';
 export async function checkAuth() {
     const currentUser = await authService.getCurrentUser();
     if (!currentUser) {
-        window.location.href = '../../index.html';
+        window.location.href = '../index.html';
         return false;
     }
     return true;
